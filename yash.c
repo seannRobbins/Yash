2c0ffcfccc3f31b5dc771de7c4c4c52182a75f38
Seann Robbins <plaez2302@gmail.com>
2023-01-22 16:11:33 -0600

    job control basics are functional

--- yash.c
+++ yash.c
@@ -9,11 +9,36 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <errno.h>
 
 #define STR_EQUAL(x1, x2)(strcmp(x1, x2) == 0)
-#define REDIR_INDEX 0
-#define REDIR_DIR 1
-#define min(a, b)(a <= b ? a : b)
+#define NONE -1
+#define MAX_LINE_LENGTH 2001
+enum job_status
+{
+    Running,
+    Stopped
+};
+
+char *job_status_strings[] = {"Running", "Stopped"};
+
+struct Process
+{
+    int pid;
+};
+
+struct Job
+{
+    int pgid;
+    int status;
+    char* cmd;
+    struct Process p1;
+    struct Process p2;
+};
+
+struct Job *jobs = NULL;
+static int job_count = 0;
+#define top_stack_job()(job_count - 1)
 
 char** parse(char *cmdline, char* delim)
 {
@@ -34,19 +59,6 @@ char** parse(char *cmdline, char* delim)
     return tokens;
 }
 
-int check_for_pipe(char** tokens)
-{
-    int index = 0;
-    while (tokens[index] != NULL)
-    {
-        if (STR_EQUAL(tokens[index], "|") == true)
-        {
-            return index;
-        }
-    }
-    return -1;
-}
-
 char* get_file_name(char** tokens, int index)
 {
     return tokens[index + 1];
@@ -63,7 +75,7 @@ int check_for_token(char** tokens, char* symbol)
         }
         index++;
     }
-    return -1;
+    return NONE;
 }
 
 void set_redirection(char** tokens)
@@ -76,9 +88,14 @@ void set_redirection(char** tokens)
     {
         fd = open(
             get_file_name(tokens, redirection_index), 
-            O_CREAT|O_RDWR, 
-            0666
+            O_RDWR, 
+            S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH
         );
+        // File does not exist
+        if (fd == -1)
+        {
+            exit(-1);
+        }
         dup2(fd, 0);
     }
     redirection_index = check_for_token(tokens, ">");
@@ -87,7 +104,7 @@ void set_redirection(char** tokens)
         fd = open(
             get_file_name(tokens, redirection_index), 
             O_CREAT|O_RDWR, 
-            0666
+            S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH
         );
         dup2(fd, 1);
     }
@@ -97,7 +114,7 @@ void set_redirection(char** tokens)
         fd = open(
             get_file_name(tokens, redirection_index), 
             O_CREAT|O_RDWR, 
-            0666
+            S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH
         );
         dup2(fd, 2);
     }
@@ -114,7 +131,7 @@ int find_smallest_redirection_index(char** tokens)
         }
         index++;
     }
-    return -1;
+    return NONE;
 }
 
 int get_number_tokens(char **tokens)
@@ -141,11 +158,13 @@ char** set_child_tokens(char  **tokens, int start_index, int end_index)
     return child_tokens;
 }
 
-int handle_child(char **tokens, bool pipe, int pfd_open, int pfd_close, int replaced_fd)
+int handle_child(char **tokens, bool pipe, int pfd_open, int pfd_close, int replaced_fd, int pgid)
 {
     int cpid = fork();
     if (cpid == 0) 
     {   
+        int exec_code;
+        setpgid(0, pgid);
         if (pipe)
         {
             dup2(pfd_open, replaced_fd);
@@ -159,40 +178,242 @@ int handle_child(char **tokens, bool pipe, int pfd_open, int pfd_close, int repl
             tokens[redirection_index] = NULL;
         }
 
-        execvp(tokens[0], tokens);
+        exec_code = execvp(tokens[0], tokens);
+        if (exec_code < 0)
+        {
+            exit(255);
+        }
     }
     return cpid;
 }
 
+void handle_ctrl_c(int sig)
+{
+    int status, pgid;
+
+    if (job_count > 0)
+    {
+        kill(-jobs[top_stack_job()].pgid, SIGKILL);
+        while (waitpid(-jobs[top_stack_job()].pgid, &status, 0) != -1) {
+        // Got a child.  Wait for more.
+        }
+        free(jobs[top_stack_job()].cmd);
+        job_count--;
+    }
+    
+    printf("\n# ");
+}
+
+void handle_ctrl_z(int sig)
+{
+    if (job_count > 0)
+    {
+        kill(jobs[top_stack_job()].pgid, SIGTSTP);
+        jobs[top_stack_job()].status = Stopped;
+    }
+    printf("\n# ");
+}
+
+void initialize_signals()
+{
+    signal(SIGINT, &handle_ctrl_c);
+    signal(SIGTSTP, &handle_ctrl_z);
+    signal(SIGTTOU, SIG_IGN);
+}
+
+void set_job_parameters(int job_index, int p1id, int p2id, int pgid, int status, char* cmd)
+{
+    jobs[job_index].p1.pid = p1id;
+    jobs[job_index].p2.pid = p2id;
+    jobs[job_index].pgid = pgid;
+    jobs[job_index].status = status;
+    if (job_index == top_stack_job())
+    {
+        jobs[job_index].cmd = malloc(MAX_LINE_LENGTH * sizeof( char ));
+        strcpy(jobs[job_index].cmd, cmd);
+    }
+    else
+    {
+        jobs[job_index].cmd = cmd;
+    }
+}
+
+void handle_jobs_cmd()
+{
+    for (int i = 0; i < job_count; i++)
+    {
+        printf("[%d] - %s       %s\n", i + 1, job_status_strings[jobs[i].status], jobs[i].cmd);
+    }
+}
+
+void handle_bg_cmd()
+{
+    int index;
+    if (job_count > 0)
+    {
+        for (index = job_count - 1; index >= 0; index--)
+        {
+            if (jobs[index].status == Stopped)
+            {
+                break;
+            }
+        }
+        kill(-jobs[index].pgid, SIGCONT);
+        jobs[index].status = Running;
+    }
+}
+
+void handle_fg_cmd()
+{
+    int status, retcpid;
+    if (job_count > 0)
+    {
+        kill(-jobs[top_stack_job()].pgid, SIGCONT);
+        jobs[top_stack_job()].status = Running;
+
+        // Wait for Yash to get terminal control back
+        printf("%s", jobs[top_stack_job()].cmd);
+        retcpid = waitpid(jobs[top_stack_job()].p2.pid, &status, WUNTRACED);
+        jobs[top_stack_job()].status = Stopped;
+        if (WEXITSTATUS(status) == 0 | WEXITSTATUS(status) == 255)
+        {
+            free(jobs[top_stack_job()].cmd);
+            job_count--;
+        }
+    }
+}
+
+void delete_dead_job(int index)
+{
+    if (jobs[index].cmd != NULL)
+    {
+        free(jobs[index].cmd);
+    }
+    for (int i = index; i < job_count - 1; i++)
+    {
+        set_job_parameters(
+            index,
+            jobs[i + 1].p1.pid,
+            jobs[i + 1].p2.pid,
+            jobs[i + 1].pgid,
+            jobs[i + 1].status,
+            jobs[i + 1].cmd
+        );
+    }
+    job_count--;
+}
+
+void periodic_reap_for_jobs()
+{
+    int index, status;
+    for (index = 0; index < job_count; index++)
+    {
+        waitpid(-jobs[index].pgid, &status, WNOHANG);
+        if (WEXITSTATUS(status) == 0)
+        {
+            delete_dead_job(index);
+        }
+    }
+}
+
 int main()
 {
-    int right_cpid, left_cpid, status;
-    char* cmdline;
-    char **tokens;
-    char **p1_tokens;
-    char **p2_tokens;
-    int pipe_index;
+    int right_cpid, left_cpid, retcpid, status;
+    char *cmdline;
+    char *cmdlinecopy = malloc(MAX_LINE_LENGTH * sizeof( char ));
+    char **tokens = NULL;
+    char **p1_tokens = NULL;
+    char **p2_tokens = NULL;
+    int pipe_index, amp_index;
     int pfd[2];
+    bool wait_flag = false;
+    initialize_signals();
     while(cmdline = readline("# "))
     {
-        tokens = parse(cmdline, " ");
-        pipe_index = check_for_token(tokens, "|");
-        if (pipe_index > -1)
+        if (cmdline[0] == 0)
         {
-            pipe(pfd);
-            p1_tokens = set_child_tokens(tokens, 0, pipe_index);
-            p2_tokens = set_child_tokens(tokens, pipe_index + 1, get_number_tokens(tokens) - 1);
-            left_cpid = handle_child(p1_tokens, true, pfd[1], pfd[0], 1);
-            right_cpid = handle_child(p2_tokens, true, pfd[0], pfd[1], 0);
+            continue;
+        }
+
+        strcpy(cmdlinecopy, cmdline);
+        tokens = parse(cmdline, " ");
+
+        if (check_for_token(tokens, "jobs") != NONE)
+        {
+            if (get_number_tokens(tokens) == 2)
+            {
+                handle_jobs_cmd();
+            }
+        }
+        else if (check_for_token(tokens, "bg") != NONE)
+        {
+            if (get_number_tokens(tokens) == 2)
+            {
+                handle_bg_cmd();
+            }
+        }
+        else if (check_for_token(tokens, "fg") != NONE)
+        {
+            if (get_number_tokens(tokens) == 2)
+            {
+                handle_fg_cmd();
+            }
         }
         else
         {
-           right_cpid = handle_child(tokens, false, -1, -1, -1);
+            amp_index = check_for_token(tokens, "&");
+            if (amp_index != NONE)
+            {
+                tokens[amp_index] = NULL;
+            }
+            pipe_index = check_for_token(tokens, "|");
+            job_count++;
+            jobs = realloc(jobs, job_count * sizeof( struct Job ));
+            if (pipe_index > -1)
+            {
+                pipe(pfd);
+                p1_tokens = set_child_tokens(tokens, 0, pipe_index);
+                p2_tokens = set_child_tokens(tokens, pipe_index + 1, get_number_tokens(tokens) - 1);
+                left_cpid = handle_child(p1_tokens, true, pfd[1], pfd[0], STDOUT_FILENO, 0);
+                right_cpid = handle_child(p2_tokens, true, pfd[0], pfd[1], STDIN_FILENO, left_cpid);
+                close(pfd[0]);
+                close(pfd[1]);
+            }
+            else
+            {
+                left_cpid = handle_child(tokens, false, NONE, NONE, NONE, 0);
+                right_cpid = left_cpid;
+            }
+            set_job_parameters(top_stack_job(), left_cpid, right_cpid, left_cpid, Running, cmdlinecopy);
+        
+            if (amp_index == NONE)
+            {
+                retcpid = waitpid(jobs[top_stack_job()].p2.pid, &status, WUNTRACED);
+                if (WEXITSTATUS(status) == 0 | WEXITSTATUS(status) == 255)
+                {
+                    if (jobs[top_stack_job()].cmd != NULL)
+                    {
+                        free(jobs[top_stack_job()].cmd);
+                    }
+                    job_count--;
+                }
+            }
+
         }
-        close(pfd[0]);
-        close(pfd[1]);
-        waitpid(right_cpid, &status, WUNTRACED);
+        periodic_reap_for_jobs();
+
+
+        // printf("Top Stack Job: %d, %d, %d\n",  jobs[top_stack_job(job_count)].pgid,  jobs[top_stack_job(job_count)].p1.pid,  jobs[top_stack_job(job_count)].p2.pid);
+        printf("Wait Status: %d, PGID: %d\n", WEXITSTATUS(status), retcpid);
+        // printf("Job Count: %d\n", job_count);
+        // for (int i = 0; i < job_count; i++)
+        // {
+        //     printf("%d. pgid: %d, p1id: %d, p2id: %d\n", i, jobs[i].pgid, jobs[i].p1.pid, jobs[i].p2.pid);
+        // }
+    
+    
     }
+    free(cmdlinecopy);
 
     if (tokens != NULL)
     {
@@ -202,8 +423,19 @@ int main()
     {
         free(p1_tokens);
     }
-    if(p2_tokens != NULL)
+    if (p2_tokens != NULL)
     {
         free(p2_tokens);
+    }
+    if (jobs != NULL)
+    {
+        for (int i = 0; i < job_count; i++)
+        {
+            if (jobs[i].cmd != NULL)
+            {
+                free(jobs[i].cmd);
+            }
+        }
+        free(jobs);
     }
 }
