b3821b01ffb54c86cfe15ca93fe12f826a90c6ac
Seann Robbins <plaez2302@gmail.com>
2023-01-26 12:24:45 -0600

    implemented sigchld handler

--- yash.c
+++ yash.c
@@ -5,8 +5,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <sys/types.h>
+#include <signal.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdbool.h>
 #include <errno.h>
@@ -38,6 +38,7 @@ struct Job
 
 struct Job *jobs = NULL;
 static int job_count = 0;
+static bool fg_flag = false;
 #define top_stack_job()(job_count - 1)
 
 char** parse(char *cmdline, char* delim)
@@ -158,12 +159,105 @@ char** set_child_tokens(char  **tokens, int start_index, int end_index)
     return child_tokens;
 }
 
+void set_job_parameters(int job_index, int p1id, int p2id, int pgid, int status, char* cmd)
+{
+    jobs[job_index].p1.pid = p1id;
+    jobs[job_index].p2.pid = p2id;
+    jobs[job_index].pgid = pgid;
+    jobs[job_index].status = status;
+    if (job_index == top_stack_job())
+    {
+        jobs[job_index].cmd = malloc(MAX_LINE_LENGTH * sizeof( char ));
+        strcpy(jobs[job_index].cmd, cmd);
+    }
+    else
+    {
+        jobs[job_index].cmd = cmd;
+    }
+}
+
+void delete_dead_job(int index)
+{
+    if (jobs[index].cmd != NULL)
+    {
+        free(jobs[index].cmd);
+    }
+    for (int i = index; i < job_count - 1; i++)
+    {
+        set_job_parameters(
+            index,
+            jobs[i + 1].p1.pid,
+            jobs[i + 1].p2.pid,
+            jobs[i + 1].pgid,
+            jobs[i + 1].status,
+            jobs[i + 1].cmd
+        );
+    }
+    job_count--;
+}
+
+void handle_waitpid_status(int retcpid, int status, int index)
+{
+    int sig = WTERMSIG(status);
+    if (retcpid > 0)
+    {
+        if (WIFEXITED(status) || WIFSIGNALED(status))
+        {
+            delete_dead_job(index);
+        }
+        else if (WIFSTOPPED(status))
+        {
+            jobs[index].status = Stopped;
+        }
+    }
+    else if (retcpid == -1)
+    {
+        printf("error occured");
+    }
+}
+
+void sigchild_handler(int sig)
+{
+    int index;
+    int retval;
+    int status; 
+
+    if (fg_flag)
+    {
+        return;
+    }
+
+    for (index = 0; index < job_count; index++)
+    {
+        retval = waitpid(jobs[index].p2.pid, &status, WNOHANG | WUNTRACED); 
+        handle_waitpid_status(retval, status, index);
+    }
+}
+
+void init_parent_signals()
+{
+    signal(SIGINT, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGCHLD, &sigchild_handler);
+}
+
+void init_child_signals()
+{
+    signal(SIGINT, SIG_DFL);
+    signal(SIGTSTP, SIG_DFL);
+    signal(SIGTTIN, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
+}
+
 int handle_child(char **tokens, bool pipe, int pfd_open, int pfd_close, int replaced_fd, int pgid)
 {
     int cpid = fork();
     if (cpid == 0) 
     {   
         int exec_code;
+        init_child_signals();
         setpgid(0, pgid);
         if (pipe)
         {
@@ -187,63 +281,14 @@ int handle_child(char **tokens, bool pipe, int pfd_open, int pfd_close, int repl
     return cpid;
 }
 
-void handle_ctrl_c(int sig)
-{
-    int status, pgid;
-
-    if (job_count > 0)
-    {
-        kill(-jobs[top_stack_job()].pgid, SIGKILL);
-        while (waitpid(-jobs[top_stack_job()].pgid, &status, 0) != -1) {
-        // Got a child.  Wait for more.
-        }
-        free(jobs[top_stack_job()].cmd);
-        job_count--;
-    }
-    
-    printf("\n# ");
-}
-
-void handle_ctrl_z(int sig)
-{
-    if (job_count > 0)
-    {
-        kill(jobs[top_stack_job()].pgid, SIGTSTP);
-        jobs[top_stack_job()].status = Stopped;
-    }
-    printf("\n# ");
-}
-
-void initialize_signals()
-{
-    signal(SIGINT, &handle_ctrl_c);
-    signal(SIGTSTP, &handle_ctrl_z);
-    signal(SIGTTOU, SIG_IGN);
-}
-
-void set_job_parameters(int job_index, int p1id, int p2id, int pgid, int status, char* cmd)
-{
-    jobs[job_index].p1.pid = p1id;
-    jobs[job_index].p2.pid = p2id;
-    jobs[job_index].pgid = pgid;
-    jobs[job_index].status = status;
-    if (job_index == top_stack_job())
-    {
-        jobs[job_index].cmd = malloc(MAX_LINE_LENGTH * sizeof( char ));
-        strcpy(jobs[job_index].cmd, cmd);
-    }
-    else
-    {
-        jobs[job_index].cmd = cmd;
-    }
-}
-
 void handle_jobs_cmd()
 {
-    for (int i = 0; i < job_count; i++)
+    int i;
+    for (i = 0; i < job_count - 1; i++)
     {
         printf("[%d] - %s       %s\n", i + 1, job_status_strings[jobs[i].status], jobs[i].cmd);
     }
+    printf("[%d] + %s       %s\n", i + 1, job_status_strings[jobs[i].status], jobs[i].cmd);
 }
 
 void handle_bg_cmd()
@@ -268,51 +313,17 @@ void handle_fg_cmd()
     int status, retcpid;
     if (job_count > 0)
     {
+        fg_flag = true;
+        tcsetpgrp(STDIN_FILENO, jobs[top_stack_job()].pgid);
         kill(-jobs[top_stack_job()].pgid, SIGCONT);
         jobs[top_stack_job()].status = Running;
 
         // Wait for Yash to get terminal control back
-        printf("%s", jobs[top_stack_job()].cmd);
         retcpid = waitpid(jobs[top_stack_job()].p2.pid, &status, WUNTRACED);
+        fg_flag = false;
+        tcsetpgrp(STDIN_FILENO, getpgid(getpid()));
+        handle_waitpid_status(retcpid, status, top_stack_job());
         jobs[top_stack_job()].status = Stopped;
-        if (WEXITSTATUS(status) == 0 | WEXITSTATUS(status) == 255)
-        {
-            free(jobs[top_stack_job()].cmd);
-            job_count--;
-        }
-    }
-}
-
-void delete_dead_job(int index)
-{
-    if (jobs[index].cmd != NULL)
-    {
-        free(jobs[index].cmd);
-    }
-    for (int i = index; i < job_count - 1; i++)
-    {
-        set_job_parameters(
-            index,
-            jobs[i + 1].p1.pid,
-            jobs[i + 1].p2.pid,
-            jobs[i + 1].pgid,
-            jobs[i + 1].status,
-            jobs[i + 1].cmd
-        );
-    }
-    job_count--;
-}
-
-void periodic_reap_for_jobs()
-{
-    int index, status;
-    for (index = 0; index < job_count; index++)
-    {
-        waitpid(-jobs[index].pgid, &status, WNOHANG);
-        if (WEXITSTATUS(status) == 0)
-        {
-            delete_dead_job(index);
-        }
     }
 }
 
@@ -327,7 +338,7 @@ int main()
     int pipe_index, amp_index;
     int pfd[2];
     bool wait_flag = false;
-    initialize_signals();
+    init_parent_signals();
     while(cmdline = readline("# "))
     {
         if (cmdline[0] == 0)
@@ -335,7 +346,7 @@ int main()
             continue;
         }
 
-        strcpy(cmdlinecopy, cmdline);
+        cmdlinecopy = strdup(cmdline);
         tokens = parse(cmdline, " ");
 
         if (check_for_token(tokens, "jobs") != NONE)
@@ -388,30 +399,14 @@ int main()
         
             if (amp_index == NONE)
             {
+                fg_flag = true;
+                tcsetpgrp(STDIN_FILENO, jobs[top_stack_job()].pgid);
                 retcpid = waitpid(jobs[top_stack_job()].p2.pid, &status, WUNTRACED);
-                if (WEXITSTATUS(status) == 0 | WEXITSTATUS(status) == 255)
-                {
-                    if (jobs[top_stack_job()].cmd != NULL)
-                    {
-                        free(jobs[top_stack_job()].cmd);
-                    }
-                    job_count--;
-                }
+                tcsetpgrp(STDIN_FILENO, getpgid(getpid()));
+                handle_waitpid_status(retcpid, status, top_stack_job());
+                fg_flag = false;
             }
-
         }
-        periodic_reap_for_jobs();
-
-
-        // printf("Top Stack Job: %d, %d, %d\n",  jobs[top_stack_job(job_count)].pgid,  jobs[top_stack_job(job_count)].p1.pid,  jobs[top_stack_job(job_count)].p2.pid);
-        printf("Wait Status: %d, PGID: %d\n", WEXITSTATUS(status), retcpid);
-        // printf("Job Count: %d\n", job_count);
-        // for (int i = 0; i < job_count; i++)
-        // {
-        //     printf("%d. pgid: %d, p1id: %d, p2id: %d\n", i, jobs[i].pgid, jobs[i].p1.pid, jobs[i].p2.pid);
-        // }
-    
-    
     }
     free(cmdlinecopy);
 
